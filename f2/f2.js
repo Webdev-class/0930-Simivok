6c0321f9eff67161dd92a73891717c4dd068ae6f
Simivok <bence8035@gmail.com>
2024-10-01 09:24:35 +0200

    Kész Feladat

--- f2/f2.js
+++ f2/f2.js
@@ -2,7 +2,20 @@ const students = require('../students.json');
 /* Írj függvényt ami feltölt egy objektumot (lehetőleg üres {}-ről induljon) 
 a klubbok nevével és tagjainak számával. (pl. {"club1": 4, "club2": 3....}) */
 function f2() {
-    //TODO
+    let club = {};
+    students.forEach(student => {
+        if (!club[student.club]) {
+            club[student.club] = 1;
+
+            
+        }
+        else{
+            club[student.club]++;
+        }
+        
+    })
+    return club;
 }
+
 
 module.exports = f2;
--- f3/f3.js
+++ f3/f3.js
@@ -4,6 +4,13 @@ const students = require('../students.json');
  (Ha jól van feltöltve a lista a false értékek száma és a null-ok száma megegyezik.
   Ha nem akkor írja ki az eltérést) */
 const f3 = () => {
-    //TODO
+    let x = 0;
+    students.forEach(student =>{
+      if (student.freeDormitory === true  && student.dormitoryPrice !== null) {
+          x++;
+      }
+
+    })
+    return x;
 }
 module.exports = f3;
